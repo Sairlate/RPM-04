0086a7f06803079f7d81918c059cd1d1da35e6b9
Sairlate <dlya.aktivacii@mail.ru>
2022-02-03 20:07:40 +0300

    ver 2

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import os
 
 def userpath():
+    path = str(input)
+    for i in userpath():
+        if os.path.exist(path):
+            print("Путь указан верно")
+    upath = os.path.abspath
 
 def dict():
+    dict = {}
 
 def dubl():
 
+
 def console():
+
