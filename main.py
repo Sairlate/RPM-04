39f86a7817f6ba3065362cdc2fdfb22a32054177
Sairlate <dlya.aktivacii@mail.ru>
2022-02-14 20:42:40 +0300

    ver 2

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import os
 
-def userpath():
-    path = str(input)
-    for i in userpath():
-        if os.path.exist(path):
-            print("Путь указан верно")
-    upath = os.path.abspath
 
-def dict():
-    dict = {}
+def userpath():
+    while True:
+        path = "C:\\abc"
+        if os.path.exists(path):
+            break
+        else:
+            print("Вы не правильно ввели путь")
+    return path
+
+def dict(path):
+    dict1 = {}
+    for i in os.listdir(path):
+        if os.path.isdir(path + i):
+            dict1[path + "\\" + i] = os.path.getsize(path + "\\" + i)
+        else:
+            dict(path + "\\" + i)
+    print(dict) 
+           
 
 def dubl():
-
+    pass
 
 def console():
+    pass
+
+dict(userpath())
+
+
+# a = {
+#     1:41,
+#     2:22,
+#     3:3
+# }
+# a[1] = "gssefd"
+# print(a[])
 
